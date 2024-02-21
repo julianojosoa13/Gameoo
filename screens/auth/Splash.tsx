623d8e45ce5ca728d3bd32aeaf3355ce0f86b64d
julianojosoa13 <josoa.juliano.rahajaharimanga@esti.mg>
2024-02-21 23:22:02 +0300

    Changed the content of splash.tsx

--- screens/auth/Splash.tsx
+++ screens/auth/Splash.tsx
@@ -1,19 +1,28 @@
-import { StyleSheet, Text, View, SafeAreaView, ImageBackground, ActivityIndicator } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView, ImageBackground, ActivityIndicator, Image } from 'react-native'
 import React, { useEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
+import LottieView from 'lottie-react-native';
 
 const Splash = () => {
   const navigation = useNavigation()
   useEffect(() => {
     setTimeout(()=>{
         navigation.replace("Onboarding")
-    },3000)
+    },3200)
   },[])
   return (
-    <SafeAreaView style={{flex: 1, justifyContent: "space-between"}}>
+    <SafeAreaView style={{flex: 1, justifyContent: "space-between", alignItems: "center"}}>
       <ImageBackground source={require('../../assets/Images/bg.png')} style={{width: "100%", height:"100%", position: "absolute"}} resizeMode='cover'/>
       <View />
-      <Text style={{fontFamily: "SF-Regular", textAlign: "center", fontSize: 18}}>Gameoo, tous droits réservés, {new Date().getFullYear()}</Text>
+      <Image source={require('../../assets/branding/LOGO-Gameoo-PNG.png')} style={{width: 220, height: 220}}/>
+      <View
+        style={{
+            alignItems: "center",
+        }}
+      >
+        <LottieView source={require('../../assets/Animations/Lotties/loading-walk.json')} autoPlay loop style={{width: 120, height: 120}}/>
+        <Text style={{fontFamily: "SF-Regular", textAlign: "center", fontSize: 18, marginBottom: 25}}>Gameoo, tous droits réservés, {new Date().getFullYear()}</Text>
+      </View>
     </SafeAreaView>
   )
 }
