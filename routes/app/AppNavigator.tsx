6e7e762950c574f4c28c98ce04d96f016c67e67e
julianojosoa13 <josoa.juliano.rahajaharimanga@esti.mg>
2024-07-15 17:16:28 +0300

    Mise a jour des pacquets

--- routes/app/AppNavigator.tsx
+++ routes/app/AppNavigator.tsx
@@ -1,36 +1,35 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, { useContext } from 'react'
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import FirstStep from '../../screens/app/FirstStep';
-import { Session } from '@supabase/supabase-js';
-import BottomTabNavigator from './BottomTabNavigator/BottomTabNavigator';
-import LoggingOut from '../../screens/app/LoggingOut';
-import AuthNavigator from '../auth/AuthNavigator';
-import SearchScreen from '../../screens/app/SearchScreen';
-import Game from '../../screens/app/Game';
-import Shop from '../../screens/app/Shop';
+import { StyleSheet, Text, View } from "react-native";
+import React, { useContext } from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import FirstStep from "../../screens/app/FirstStep";
+import { Session } from "@supabase/supabase-js";
+import BottomTabNavigator from "./BottomTabNavigator/BottomTabNavigator";
+import LoggingOut from "../../screens/app/LoggingOut";
+import AuthNavigator from "../auth/AuthNavigator";
+import SearchScreen from "../../screens/app/SearchScreen";
+import Game from "../../screens/app/Game";
+import Shop from "../../screens/app/Shop";
 
 const Stack = createNativeStackNavigator();
 
 const AppNavigator = ({ session }: { session: Session | null }) => {
-
   return (
     <Stack.Navigator
       screenOptions={{
-        headerShown: false
+        headerShown: false,
       }}
     >
-      <Stack.Screen name='FirstStep' component={FirstStep} />
+      <Stack.Screen name="FirstStep" component={FirstStep} />
       <Stack.Screen name="Tabs" component={BottomTabNavigator} />
       <Stack.Screen name="LogOut" component={LoggingOut} />
-      <Stack.Screen name="Auth" component={AuthNavigator} />
+      {/* <Stack.Screen name="Auth" component={AuthNavigator} /> */}
       <Stack.Screen name="SearchScreen" component={SearchScreen} />
       <Stack.Screen name="Game" component={Game} />
       <Stack.Screen name="Shop" component={Shop} />
     </Stack.Navigator>
-  )
-}
+  );
+};
 
-export default AppNavigator
+export default AppNavigator;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- routes/auth/AuthNavigator.tsx
+++ routes/auth/AuthNavigator.tsx
@@ -1,14 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Onboarding from '../../screens/auth/Onboarding';
-import Login from '../../screens/auth/Login';
-import Splash from '../../screens/auth/Splash';
-import Register from '../../screens/auth/Register';
-import FInalStep from '../../screens/app/FirstStep';
-import OTPVerification from '../../screens/auth/OTPVerification';
-import AppNavigator from '../app/AppNavigator';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Onboarding from "../../screens/auth/Onboarding";
+import Login from "../../screens/auth/Login";
+import Splash from "../../screens/auth/Splash";
+import Register from "../../screens/auth/Register";
+import FInalStep from "../../screens/app/FirstStep";
+import OTPVerification from "../../screens/auth/OTPVerification";
+import AppNavigator from "../app/AppNavigator";
 
 const Stack = createNativeStackNavigator();
 
@@ -16,19 +16,19 @@ const AuthNavigator = () => {
   return (
     <Stack.Navigator
       screenOptions={{
-        headerShown: false
+        headerShown: false,
       }}
     >
-      <Stack.Screen name='Onboarding' component={Onboarding} />
+      <Stack.Screen name="Onboarding" component={Onboarding} />
       <Stack.Screen name="Login" component={Login} />
-      <Stack.Screen name='Register' component={Register} />
-      <Stack.Screen name='FinalStep' component={FInalStep} />
+      <Stack.Screen name="Register" component={Register} />
+      <Stack.Screen name="FinalStep" component={FInalStep} />
       <Stack.Screen name="OTPVerif" component={OTPVerification} />
       <Stack.Screen name="AppNavigator" component={AppNavigator} />
     </Stack.Navigator>
-  )
-}
+  );
+};
 
-export default AuthNavigator
+export default AuthNavigator;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
